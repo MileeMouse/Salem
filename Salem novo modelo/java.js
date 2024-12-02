0e559104030bc04508551f2805875930e20e24a7
IHumbertoCastroI <humberto.c.castro@gmail.com>
2024-12-02 15:00:23 -0300

    Atualização de endereços e PDF
    - Falta o calendario academico

--- Salem novo modelo/java.js
+++ Salem novo modelo/java.js
@@ -68,7 +68,7 @@ prevButton.addEventListener("click", () => {
 // Alterador de mapas
 
 var mapas = [
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3600.2124001557386!2d-49.30195038227947!3d-25.531300932705694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dcfc8c1ee754a9%3A0x12fbab5124e27157!2sCuritiba%20-%20State%20of%20Paran%C3%A1%2C%2081690-400!5e0!3m2!1sen!2sbr!4v1715798988292!5m2!1sen!2sbr&hl=pt&language=pt",
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3600.2770439661463!2d-49.2986737!3d-25.529147!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dcfc8c10f07087%3A0x1951d4e14f163740!2sVia%20R.%20Londrina%2C%20Curitiba%20-%20PR%2C%2082590-300!5e0!3m2!1sen!2sbr!4v1733161790182!5m2!1sen!2sbr",
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3600.2124001557386!2d-51.16801278234866!3d-29.924038510868647!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x951976d2d16c3dbb%3A0xb67eb24c1a1686c3!2sCanoas%20-%20RS%2C%2092330-500!5e0!3m2!1sen!2sbr!4v1715799457602!5m2!1sen!2sbr&hl=pt&language=pt",
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.007319673063!2d-46.622436785006335!3d-23.686298670570016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce4c59ebedf021%3A0x146aa3fbbcb36d2c!2sDiadema%20-%20SP%2C%2009911-160!5e0!3m2!1sen!2sbr!4v1715799556884!5m2!1sen!2sbr&hl=pt&language=pt",
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3974.2546022686216!2d-63.03383348523882!3d-9.909992492926657!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93cd5e3e2d33a25f%3A0xf816c29b38d0e20!2sR.%20Cardeal%2C%201785%20-%20Setor%202%2C%20Ariquemes%20-%20RO%2C%2076873-200%2C%20Brasil!5e0!3m2!1spt-BR!2sus!4v1715801939944!5m2!1spt-BR!2sus&hl=pt&language=pt",
